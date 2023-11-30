f3eee801c7db06feaff1ba844a378692a00837b3
Benjamin Galland <galland.benjamin98@gmail.com>
2023-11-30 11:54:52 +0100

    done with the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+
+puts "Clear movies..."
+Movie.destroy_all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+response = JSON.parse(response)
+results = response["results"]
+results.each do |result|
+  puts result["title"]
+  Movie.create(
+    title: result["title"],
+    overview: result["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{result["poster_path"]}",
+    rating: result["vote_average"].round(2)
+  )
+end
