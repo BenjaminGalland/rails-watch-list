2f2c3da76fba5f2b5949bb9475f20a372c2d8002
Benjamin Galland <galland.benjamin98@gmail.com>
2023-12-01 12:40:44 +0100

    test cloudinary

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,7 +13,7 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    if @list.save()
+    if @list.save!
       redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
@@ -27,6 +27,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
